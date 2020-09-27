b20bdfd5b2be2aea7d43a447145bd36caca71030
pitsanubomb <pitsanubomb@gmail.com>
2020-09-27 23:44:44 +0700

    :fire: Setup client :rocket:

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,12 @@
-import './plugins/bootstrap-vue';
-
-import '@babel/polyfill';
-import 'mutationobserver-shim';
-
-import { createApp } from 'vue';
+import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
