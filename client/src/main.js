668e4dba12ac6fd4ee2d9eacfa83a332ad0a2140
pitsanubomb <pitsanubomb@gmail.com>
2020-09-28 00:33:27 +0700

    :fire: Add Bootstrap :rocket;

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import '@babel/polyfill';
+import 'mutationobserver-shim';
 import Vue from 'vue';
+import './plugins/bootstrap-vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
