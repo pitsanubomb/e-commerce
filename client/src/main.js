badd879c4c98c49a3705aabd2b55e6a3cc426fe2
pitsanubomb <pitsanubomb@gmail.com>
2020-09-29 00:13:53 +0700

    :fire: Add composition api :rocket:

--- client/src/main.js
+++ client/src/main.js
@@ -2,12 +2,14 @@
 import '@babel/polyfill';
 import 'mutationobserver-shim';
 import Vue from 'vue';
+import VueCompositionAPI from '@vue/composition-api';
 import './plugins/bootstrap-vue';
 
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+Vue.use(VueCompositionAPI);
 Vue.config.productionTip = false;
 
 new Vue({
