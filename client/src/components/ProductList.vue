962b526962fa475d77db36ef51095516106d6f42
pitsanubomb <pitsanubomb@gmail.com>
2020-09-29 22:16:52 +0700

    :fire: Add image and rewrite ui  :rocket:

--- client/src/components/ProductList.vue
+++ client/src/components/ProductList.vue
@@ -1,30 +1,26 @@
 <template>
   <div class="container-fulid mt-3 mx-3">
-    <b-card-group deck>
+    <b-card-group>
       <b-card
-        no-body
         v-for="product in products"
         :key="product.id"
-        class="overflow-hidden"
-        style="max-width: 540px"
+        :title="product.name"
+        border-variant="Light"
+        style="max-width: 20rem"
+        img-src="https://placekitten.com/380/200"
+        img-alt="Image"
+        img-top
       >
-        <b-row no-gutters>
-          <b-col md="6">
-            <b-card-img
-              src="https://picsum.photos/400/400/?image=20"
-              alt="Image"
-              class="rounded-0"
-            ></b-card-img>
-          </b-col>
-          <b-col md="6">
-            <b-card-body title="Horizontal Card">
-              <b-card-text>
-                This is a wider card with supporting text as a natural lead-in
-                to additional content. This content is a little bit longer.
-              </b-card-text>
-            </b-card-body>
-          </b-col>
-        </b-row>
+        <b-card-text
+          ><b-row>
+            <b-col>฿{{ product.prize }}</b-col>
+            <b-img
+              src="@/assets/free-free-shipping-icon-2048-thumb.png"
+              style="width: 40px; height: 40px"
+              rounded
+              alt="Rounded image"
+            ></b-img> </b-row
+        ></b-card-text>
       </b-card>
     </b-card-group>
   </div>
@@ -37,7 +33,7 @@ export default {
   setup() {
     const products = ref([]);
 
-    const apiUrl = 'http://localhost:8080/api/product/all';
+    const apiUrl = 'http://localhost:3000/api/product/all';
 
     async function getProductList() {
       const res = await fetch(apiUrl);
