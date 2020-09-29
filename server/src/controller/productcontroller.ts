cd868fbbbe9d7e789d50da8737710059c1d41ce1
pitsanubomb <pitsanubomb@gmail.com>
2020-09-29 23:27:35 +0700

    :fire: Get One product controller :rocket:

--- server/src/controller/productcontroller.ts
+++ server/src/controller/productcontroller.ts
@@ -5,11 +5,26 @@ class ProductController {
   static create = async (req: Request, res: Response) => {
     const repo = getRepository(Product);
     try {
-      const product = await repo.create(req.body);
+      const product = repo.create(req.body);
       await repo.save(req.body);
       res.send({ message: "Save success", data: product });
     } catch (error) {
       res.status(500).send({ message: "Save fail", error: error });
+    }
+  };
+
+  static getOneById = async (req: Request, res: Response) => {
+    const id: any = req.params.id;
+
+    const repo = getRepository(Product);
+    try {
+      const product = await repo.findOneOrFail(id);
+      res.send({ message: "Fetch success", data: product });
+    } catch (error) {
+      res.status(404).send({
+        message: "User not found",
+        error: error,
+      });
     }
   };
 
