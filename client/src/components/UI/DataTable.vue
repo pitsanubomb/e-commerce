09c6fe44d4cdf295e75187012b3dc1b023f2240f
pitsanubomb <pitsanubomb@gmail.com>
2020-09-30 23:05:58 +0700

    :fire: Add UI Datatable  :rocket:

---
+++ client/src/components/UI/DataTable.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-table striped hover :items="data" :fields="fields">
+      <template v-if="fields.detail" v-slot:cell(detail)="row">
+        <b-button size="sm" @click="row.toggleDetails" class="mr-2">
+          {{ row.detailsShowing ? "Hide" : "Show" }} Detail
+        </b-button>
+      </template>
+      <template v-slot:cell(Edit_Delete)="row">
+        <b-button
+          size="sm"
+          @click="editData(row.item.id)"
+          variant="success"
+          class="mr-2"
+        >
+          Edit
+        </b-button>
+        <b-button size="sm" variant="danger" class="mr-2"> Delete </b-button>
+      </template>
+      <template v-slot:row-details="row">
+        <b-card>
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>Name:</b></b-col>
+            <b-col>{{ row.item.username }}</b-col>
+          </b-row>
+          <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button>
+        </b-card>
+      </template>
+    </b-table>
+  </div>
+</template>
+<script>
+export default {
+  name: 'DataTable',
+  props: {
+    data: {
+      type: Array,
+    },
+    fields: {
+      type: Array,
+    },
+  },
+  methods: {
+    editData(id) {
+      this.$emit('edit-id', id);
+    },
+  },
+};
+</script>
